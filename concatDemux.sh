41a008ef607075b96e4d7a2cefe605d7784f1546
mskey <matthewselkirk@gmail.com>
2019-10-20 09:59:36 +0200

    ffmpeg concat safe path

--- concatDemux.sh
+++ concatDemux.sh
@@ -1,3 +1,3 @@
 # with a bash for loop
-for f in ./*.mp4; do echo "file '$f'" >> mp4ToConcat.txt; done
-#ffmpeg -f concat -i inputs.txt -c copy output.mp4
+for f in ./*.MP4; do echo "file '$f'" >> mp4ToConcat.txt; done
+ffmpeg -f concat -safe 0  -i mp4ToConcat.txt -c copy output.mp4
