27b6bead89b8ac0977e40d7ec4220e402309fea4
mattselkey <matt@mattselkey.com>
2019-10-20 16:11:47 +0200

    formatting

--- concatDemux.sh
+++ concatDemux.sh
@@ -21,11 +21,12 @@ select yne in "Yes" "No" "Exit"; do
 	esac
 done
 
-#see https://superuser.com/questions/1296374/best-settings-for-ffmpeg-with-nvenc
+# ENCODE
+# Get nvenc commands - ffmpeg -codecs | grep nvenc
+# see https://superuser.com/questions/1296374/best-settings-for-ffmpeg-with-nvenc
 # and https://superuser.com/questions/1236275/how-can-i-use-crf-encoding-with-nvenc-in-ffmpeg/1236387
 # and https://ntown.at/de/knowledgebase/cuda-gpu-accelerated-h264-h265-hevc-video-encoding-with-ffmpeg/
-#-aspect 16:9 -s 1920x1080 http://ffmpeg.org/ffmpeg-filters.html#scale
-#audio "-c:a aac -b:a 128k -ac 2"
-# disable audio -an 
-# commands - ffmpeg -codecs | grep nvenc
+# -aspect 16:9 -s 1920x1080 http://ffmpeg.org/ffmpeg-filters.html#scale
+# audio "-c:a aac -b:a 128k -ac 2", disable audio -an 
+
 ffmpeg -threads 6 -i $OUTPUTFILE -c:v h264_nvenc -preset slow -s 1920x1080 -b:v 9M -maxrate:v 12M -bufsize:v 15M -rc:v vbr_hq -cq 25 -an $FINALOUTPUT
