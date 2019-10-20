a6aec9043ada900c06b808a1c1e82d70b3b087e8
mskey <matthewselkirk@gmail.com>
2019-10-20 11:51:39 +0200

    ffmpeg params test

--- concatDemux.sh
+++ concatDemux.sh
@@ -1,3 +1,14 @@
-# with a bash for loop
-for f in ./*.MP4; do echo "file '$f'" >> mp4ToConcat.txt; done
-ffmpeg -f concat -safe 0  -i mp4ToConcat.txt -c copy output.mp4
+#!/bin/bash
+INPUTFILE="mp4ToConcat.txt"
+OUTPUTFILE="ConcatOutput.mp4"
+FINALOUTPUT="FinalOutput.mp4"
+#emtpy file
+> mp4ToConcat.txt;
+for f in ./*.MP4; do echo "file '$f'" >> $INPUTFILE; done
+#ffmpeg -f concat -safe 0  -i $INPUTFILE -c copy $OUTPUTFILE
+#se https://superuser.com/questions/1296374/best-settings-for-ffmpeg-with-nvenc
+#-aspect 16:9 -s 1920x1080 http://ffmpeg.org/ffmpeg-filters.html#scale
+#audio "-c:a aac -b:a 128k -ac 2"
+# disable audio -an 
+ffmpeg -threads 4 -i $OUTPUTFILE -c:v h264_nvenc -preset slow -s 1920x1080 -b:v 6M -maxrate:v 20M /
+-bufsize:v 15M -rc:v vbr_hq -cq 29 -an $FINALOUTPUT
